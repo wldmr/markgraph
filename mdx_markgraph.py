0663c12429da9793ef21ccb8e773582f37c11029
Andreas Waldenburger <andreas@waldenburger.name>
2015-03-23 03:19:55 +0100

    Added Outline-Treeprocessor.
    We now get nested sections, as we would like them for
    graphs.

--- mdx_markgraph.py
+++ mdx_markgraph.py
@@ -13,7 +13,7 @@ from dot import call_dot
 
 
 class Regex:
-    graph_filename = r'\bgraph_(?P<substring>.+?)\.(?P<filetype>.+?)\b'
+    graph_filename = r'\b(?P<filename>graph_(?P<substring>.+?)\.(?P<filetype>.+?))\b'
 
 
 class GraphNamePattern(SimpleTextPattern):
@@ -21,16 +21,38 @@ class GraphNamePattern(SimpleTextPattern):
     history = set()
 
     def handleMatch(self, m):
-        tup = self.tup(m.group(2), m.group('substring'), m.group('filetype'))
+        tup = self.tup(m.group('filename'), m.group('substring'), m.group('filetype'))
         self.history.add(tup)
         #return SimpleTagPattern.handleMatch(self, m)
 
 
+class MarkgraphOutlineProcessor(Treeprocessor):
+    def __init__(self, md, depth=1):
+        super(MarkgraphOutlineProcessor, self).__init__(md)
+        self.depth = depth
+        self.tag = 'h'+str(depth)
+
+    def run(self, root):
+        self.make_sections(root, 1)
+
+    def make_sections(self, root, depth):
+        tag = 'h'+str(depth)
+        section = root
+        sections = []
+        for elem in root.findall('./*'):
+            if elem.tag == tag:
+                section = etree.SubElement(root, 'section')
+                section.set('title', elem.text)
+                elem.tag = "h1"
+                sections.append(section)
+            root.remove(elem)
+            section.append(elem)
+
+        for newroot in sections:
+            self.make_sections(newroot, depth+1)
+
+
 class MarkgraphTreeProcessor(Treeprocessor):
-
-    headtags = set('h'+str(i) for i in range(1,6))
-
-    margraphtags = set(('markgraph-node', 'markgraph-filename', 'markgraph-reference'))
 
     def run(self, root):
         # collect edges
@@ -38,8 +60,9 @@ class MarkgraphTreeProcessor(Treeprocessor):
         cluster = root
         context = None
         for elem in root.iter():
-            if elem.tag in self.headtags:
+            if elem.tag == "section":
                 cluster = elem
+                elem.set("markgraph-type", "cluster")
             elif elem.tag == "a":
                 href = elem.get('href')
                 if not href or href.startswith("?"):  # We're linking two graph nodes
@@ -48,15 +71,24 @@ class MarkgraphTreeProcessor(Treeprocessor):
                     edges.add((context.text, elem.text))
                 elif href.startswith("!"):  # A node definition
                     context = elem
-                    elem.set('cluster', cluster)
+                    elem.set('markgraph-cluster', cluster.get('title'))
+                    elem.set('markgraph-type', "node")
                 elif href.startswith(":"):  # We're switching context
                     context = elem
 
-        # Output graph
-        string = """digraph {{ {edges} }}""".format(
-            edges = '\n'.join('"{}" -> "{}";'.format(*e) for e in edges),
-        )
-        call_dot("test.svg", 'svg', string)
+        # Output graphs
+        for cluster in root.iterfind(".//section[@markgraph-type='cluster']"):
+            for info in GraphNamePattern.history:
+                if info.query in cluster.get('title'):
+                    self.output(cluster, info)
+            #string = """digraph {{ {edges} }}""".format(
+            #    edges = '\n'.join('"{}" -> "{}";'.format(*e) for e in edges),
+            #)
+            #call_dot("test.svg", 'svg', string)
+
+    def output(self, cluster, info):
+        nodes = cluster.findall("./a[@markgraph-type='node']")
+        subclusters = cluster.findall("./*[@markgraph-type='cluster']")
 
 
 
@@ -67,6 +99,7 @@ class MarkgraphExtension(Extension):
         graph_def = GraphNamePattern(Regex.graph_filename, "markgraph-filename")
         md.inlinePatterns.add('markgraph_graph', graph_def, '_begin')
 
+        md.treeprocessors['outline'] = MarkgraphOutlineProcessor(md)
         md.treeprocessors['markgraph'] = MarkgraphTreeProcessor(md)
 
 
